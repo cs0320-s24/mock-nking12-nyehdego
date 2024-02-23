bcba77bf3f2c6590e24951b32cdef03535e1b225
Natalie <natalie_king@brown.edu>
2024-02-23 16:39:01 -0500

    pair programming: added mocked data

--- mockedJson.ts
+++ mockedJson.ts
@@ -2,3 +2,16 @@ const mocked_data_1 = [
     [1,2,3,4,5],
     [6,7,8,9,10]
 ]
+
+const mocked_data_2 = [
+    ["The", "sun", "shines"],
+    ["Hello", "Tim", "Bye"],
+    ["Red", "Yellow", "Blue"]
+]
+
+const mocked_data_3 = []
+
+const mocked_data_4 = [
+  ["Red", "Orange", "Yellow"],
+  ["Green", "Blue"]
+];
