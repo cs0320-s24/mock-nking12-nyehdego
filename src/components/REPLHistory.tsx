8f6caaf105653368ec0b3fb71660ec2189945655
nyehdego <nahum_yehdego@brown.edu>
2024-02-26 14:39:08 -0500

    feat: created CommandOutput function in REPLHistory

--- src/components/REPLHistory.tsx
+++ src/components/REPLHistory.tsx
@@ -3,9 +3,43 @@ import '../styles/main.css';
 interface REPLHistoryProps{
     // TODO: Fill with some shared state tracking all the pushed commands
     // CHANGED
-    history: string[]
+    // history: string[]
+
+    history: { command: string; output: string | JSX.Element }[];
+    cur_Mode: "brief" | "verbose";
+    currentFilepath: any[][] | null;
 }
 export function REPLHistory(props : REPLHistoryProps) {
+    function commandOutput(command: string): any[][] | string | JSX.Element {
+        if (command.startsWith("load_csv: ")) {
+            const filepath = command.slice("load_csv: ".length).trim();
+      
+            const dataMap: { [index: string]: any } = {
+                // insert mocked data into the map
+                // something like "dataset1.csv: datasets.CSVData"
+
+            };
+
+            if (dataMap[filepath]) {
+                return `"${filepath}" successfully loaded`;
+
+            } else {
+                return `ERROR: "${filepath}" is not valid. Try again.`
+            }
+    } else if (command === "view") {
+        if (props.currentFilepath) {
+            return (
+                //something like:
+                // <div>
+                //    <Table data = {props.currentFilepath}/>
+                // </div>
+                // where Table is imported
+            );
+        } else {
+            return "";
+        }
+    }
+
     return (
         <div className="repl-history" aria-label='repl-history'>
             {/* This is where command history will go */}
