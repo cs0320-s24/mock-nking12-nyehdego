05d8b3a4bb5a2c4699d0296106a72e2d6419e695
Natalie <natalie_king@brown.edu>
2024-02-29 11:57:57 -0500

    feat: error handling for unrecognized query

--- src/components/REPLFunction.tsx
+++ src/components/REPLFunction.tsx
@@ -107,8 +107,12 @@ function handleSearch(args: Array<string>) : string | string[][] {
     const col = args[0]
     const value = args[1]
     const query : string = col + " " + value
-    return queryMap[loadedFileName][query]
+    if (queryMap[loadedFileName][query]) {
+      return queryMap[loadedFileName][query];
+    } else {
+      return "Error: unrecognized query"
     }
+}
 }
 
 
